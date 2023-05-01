81b76916b4b9d07130f6dbc5b0f0c20ecf2b821d
Paez11 <victorpaez117@gmail.com>
2023-05-01 20:32:29 +0200

    dark theme implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+//Ngx-translate
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+//Animations
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Components routing
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+//Components
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -15,6 +22,12 @@ import { Error404Component } from './pages/error404/error404.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DownloadComponent } from './pages/download/download.component';
 import { FixedNavbarDirective } from './directives/fixed-navbar.directive';
+
+//Angular-material
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -36,6 +49,12 @@ export function createTranslateLoader(http: HttpClient) {
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
+
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule,
+
     HttpClientModule,
     TranslateModule.forRoot({
       loader: {
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/theme';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isDarkTheme: boolean;
+
+  constructor(private theme: ThemeService){
+    this.theme.initTheme();
+    this.isDarkTheme = this.theme.isDarkMode();
+  }
+
+  ngOnInit(): void {
+  }
+
+  public toggleTheme(){
+    this.theme.update(this.isDarkTheme ? 'light-theme' : 'dark-theme')
+      .then(() => {
+        this.isDarkTheme = this.theme.isDarkMode();
+      });
+  }
 
 }

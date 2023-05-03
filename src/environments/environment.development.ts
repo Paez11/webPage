a9b09c7d4624b801b8898b1ddd3562950951daac
Paez11 <victorpaez117@gmail.com>
2023-05-04 01:28:46 +0200

    ready for deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: false
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyDrwcMyW466Na06OCH72Pjj3KLw2fXmV-I",
+        authDomain: "v11games.firebaseapp.com",
+        projectId: "v11games",
+        storageBucket: "v11games.appspot.com",
+        messagingSenderId: "1034515155177",
+        appId: "1:1034515155177:web:df080e2823ba922ab5c878",
+        measurementId: "G-M2CYMY4TEC"
+    }
 };
